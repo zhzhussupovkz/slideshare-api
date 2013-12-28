9074e511172b23fe2b0883ddafe29a85d51a9b51
zhzhussupovkz <zhzhussupovkz@gmail.com>
2013-12-28 19:38:25 +0600

    add error codes array and method

--- lib/SlideShareAPI.php
+++ lib/SlideShareAPI.php
@@ -110,6 +110,39 @@ class SlideShareAPI {
 		return $final;
 	}
 
+	//get errors by code
+	private function getError($code) {
+		$errors = array(
+			'0' => 'No API Key Provided',
+			'1' => 'Failed API validation',
+			'2' => 'Failed User authentication',
+			'3' => 'Missing title',
+			'4' => 'Missing file for upload',
+			'5' => 'Blank title',
+			'6' => 'Slideshow file is not a source object',
+			'7' => 'Invalid extension',
+			'8' => 'File size too big',
+			'9' => 'SlideShow Not Found',
+			'10' => 'User Not Found',
+			'11' => 'Group Not Found',
+			'12' => 'No Tag Provided',
+			'13' => 'Tag Not Found',
+			'14' => 'Required Parameter Missing',
+			'15' => 'Search query cannot be blank',
+			'16' => 'Insufficient permissions',
+			'17' => 'Incorrect parameters',
+			'70' => 'Account already linked',
+			'71' => 'No linked account found',
+			'72' => 'User not created',
+			'73' => 'Invalid Application ID',
+			'74' => 'Login already exists',
+			'75' => 'EMail already exists',
+			'99' => 'Account Exceeded Daily Limit',
+			'100' => 'Your Account has been blocked',
+			);
+		return new Exception($this->error[$code]);
+	}
+
 	//function for set format
 	public function setDataFormat($format) {
 		$this->format = $format;
